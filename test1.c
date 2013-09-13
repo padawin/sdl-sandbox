408264d8a85de4f1ded50d5e9645b72ed2923965
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-09-14 01:33:16 +0200

    window creation moved in a function

--- test1.c
+++ test1.c
@@ -3,6 +3,7 @@
 #include <SDL/SDL.h>
 
 void pause();
+SDL_Surface* createWindow();
 
 int main(int argc, char *argv[])
 {
@@ -12,22 +13,17 @@ int main(int argc, char *argv[])
 	}
 
 	SDL_Surface *screen;
-
-	// window opening
-	screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+	screen = createWindow("Title");
 
 	if (screen == NULL) {
-		fprintf(stderr, "An error occured during the creation of the window: %s\n", SDL_GetError());
+		SDL_Quit();
 		exit(EXIT_FAILURE);
 	}
-	// window title
-	SDL_WM_SetCaption("Test title", NULL);
 
 	// main loop
 	pause();
 
 	SDL_Quit();
-
 	return EXIT_SUCCESS;
 }
 
@@ -45,4 +41,21 @@ void pause()
 				continuer = 0;
 		}
 	}
+}
+
+SDL_Surface* createWindow(char* title)
+{
+	SDL_Surface *screen;
+
+	// window title
+	SDL_WM_SetCaption(title, NULL);
+	// window opening
+	screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+
+	if (screen == NULL) {
+		fprintf(stderr, "An error occured during the creation of the window: %s\n", SDL_GetError());
+		return NULL;
+	}
+
+	return screen;
 }
